b4db45e66f5d96d32002195a3b2fbfb54d5cc5da
Zoe Zebedee <zoezebedee1@gmail.com>
2024-06-05 17:52:36 +0100

    Commented codes

---
+++ app/component/constants/userInfo.tsx
@@ -0,0 +1,65 @@
+// const user = {
+//     firstName: "Jessica",
+//     lastName: "Randall",
+//     image: "https://placehold.co/300x300/8133F1/FFFFFF/png?text=AG",
+//     location: {
+//         street: null,
+//         city: "London",
+//         country: "United Kingdom"
+//     },  
+//     bio: "Front-end developer and avid reader.",
+//     socials: [
+//         {
+//             name: "Github",
+//             link: "https://github.com/jess"
+//         },
+//         {
+//             name: "Frontend Mentor",
+//             link: "https://fm.com/jess"
+//         },
+//         {
+//             name: "LinkedIn",
+//             link: "https://linkedin.com/jess"
+//         },
+//         {
+//             name: "Twitter",
+//             link: "https://twitter.com/jess"
+//         },
+//     ]
+// }
+
+
+{/* {user.map((user, index) => (
+              <div key={index}>
+                <div>{user.image}</div>
+                
+              </div>
+            ))} */}
+// {
+//     user.map((users, index) => (
+//         <div key={index}>
+//             <img src={users.image} className="rounded-full w-32 h-32" />
+//             <h1 className="text-2xl font-bold">{users.firstName} {users.lastName}</h1>
+//             <p className="text-gray-500">{user.bio}</p>
+//             <div className="flex flex-col items-center justify-center">
+
+//                 {/* {user.socials.map((social, index) => (
+//                         <div key={index}>
+//                           <a href={social.link} target="_blank" rel="noreferrer">
+                            
+//                           </a>
+//                         </div>
+//                       ))} */}
+//                 {
+//                     user.socials.map((social, index)) => (
+//                 <div key={index}>
+//                     <a href={social.} target="_blank" rel="noreferrer">
+//                         <img src={social.icon} className="w-10 h-10" />
+//                     </a>
+//                 </div>
+//                 )
+//                       }
+//             </div>
+//         </div>
+//     ))
+// }
